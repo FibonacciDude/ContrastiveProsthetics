ae2f41f4a664a0349d555bb7c7e8ee4a566b0e88
adelmo <amomad123@gmail.com>
2022-01-01 14:16:56 -0500

    dataloader load data complete with numerically stable normalization

--- code/constants.py
+++ code/constants.py
@@ -2,7 +2,7 @@ PEOPLE_D2=list(range(40))
 PEOPLE_D3=[2,3,4,5,8,9]
 MAX_PEOPLE_D2=len(PEOPLE_D2)
 MAX_PEOPLE_D3=len(PEOPLE_D3)
-PEOPLE_D3=[pl-1+MAX_PEOPLE_D2 for pl in PEOPLE_D3] # adjust to be from 40-...
+PEOPLE_D3=[pl+MAX_PEOPLE_D2-1 for pl in PEOPLE_D3] # adjust to be from 40-...
 MAX_PEOPLE=MAX_PEOPLE_D2+MAX_PEOPLE_D3
 REPS=[1,3,4,6,2,5]
 TRAIN_REPS=REPS[:4]
@@ -14,12 +14,12 @@ TASK_DIST=[17,23]
 MAX_TASKS=sum(TASK_DIST)
 # In ms.
 TOTAL_WINDOW_SIZE=150
-WINDOW_MS=50
+WINDOW_MS=15
 # skip by 2 to get 1 frame per ms
 Hz=2000
 assert TOTAL_WINDOW_SIZE % WINDOW_MS == 0, "Window ms does not fit into total window length"
 AMT_WINDOWS=TOTAL_WINDOW_SIZE//WINDOW_MS
 WINDOW_SIZE=int(Hz*(TOTAL_WINDOW_SIZE/1000))
-GLOVE_DIM=22
+GLOVE_DIM=22 #-2 # no wrist sensors
 EMG_DIM=12
 ACC_DIM=EMG_DIM*3
--- code/dataloader.py
+++ code/dataloader.py
@@ -3,13 +3,17 @@
 import torch
 import torch.utils.data as data
 import scipy.io as sio
+from scipy import signal
 from constants import *
 import pandas as pd
 import time
 import numpy as np
 import tqdm
+from utils import RunningStats
 
 torch.manual_seed(42)
+np.set_printoptions(precision=4, suppress=True)
+torch.set_printoptions(precision=4)
 
 def get_np(dbnum, p_dir, n):
     E_mat=sio.loadmat("../db%s/s%s/S%s_E%s_A1"%(dbnum,p_dir,p_dir,n))
@@ -33,6 +37,7 @@ class DB23(data.Dataset):
         self.NEW_PEOPLE=new_people
         self.NEW_TASKS=new_tasks
         self.MAX_PEOPLE_TRAIN=MAX_PEOPLE-new_people
+        self.MAX_TASK_TRAIN=MAX_TASKS-self.NEW_TASKS
 
         # DO NOT take last *new_people*
         self.people_rand_d2=torch.randperm(MAX_PEOPLE_D2, device=self.device)
@@ -51,27 +56,11 @@ class DB23(data.Dataset):
             length=self.MAX_PEOPLE_TRAIN*(MAX_TASKS-self.NEW_TASKS)*MAX_TEST_REPS+(MAX_PEOPLE)*MAX_TASKS*MAX_REPS
         return length * AMT_WINDOWS # amt windows per task in 150 ms total window
 
-    def get_stim_rep(self, stimulus, repetition): # stimulus from 1-40, repetition from 1-6
-        global TASK_DIST
-        TASK_DIST=np.array(TASK_DIST)
-        ex=np.searchsorted(TASK_DIST.cumsum(), stimulus)
-        emg, acc, glove, stim, rep = self.Es[ex]
-
-        # emg, acc, glove, stimulus, repetition
-        stim_mask, rep_mask=(stim==stimulus), (rep==(0 if stimulus==0 else repetition))
-        mask=(stim_mask&rep_mask).squeeze()
-        #return mask,ex
-        emg_=emg[mask][self.time_mask]
-        acc_=acc[mask][self.time_mask]
-        acc_=acc_.reshape(AMT_WINDOWS, -1, ACC_DIM).mean(1)
-        glove_=glove[mask][self.time_mask]
-        return emg_, acc_, glove_
-
     def load_subject(self, subject):
         global MAX_PEOPLE_D2
         dbnum="3" if subject >= MAX_PEOPLE_D2 else "2"
-        if dbnum=="2":
-            person %= MAX_PEOPLE_D2
+        if dbnum=="3":
+            subject %= MAX_PEOPLE_D2
         p_dir=str(subject+1)
         #load and normalize
         EMG=torch.load(self.path+'db%s/s%s/emg.pt'%(dbnum,p_dir))
@@ -79,11 +68,11 @@ class DB23(data.Dataset):
         GLOVE=torch.load(self.path+'db%s/s%s/glove.pt'%(dbnum,p_dir))
         return EMG,ACC,GLOVE
 
-    def save(self, subject, tensors):
+    def save_subject(self, subject, tensors):
         EMG, ACC, GLOVE = tensors
         global MAX_PEOPLE_D2
         dbnum="3" if subject >= MAX_PEOPLE_D2 else "2"
-        if dbnum=="2":
+        if dbnum=="3":
             subject %= MAX_PEOPLE_D2
         p_dir=str(subject+1)
         #save
@@ -91,94 +80,168 @@ class DB23(data.Dataset):
         torch.save(ACC, self.path+'db%s/s%s/acc.pt'%(dbnum,p_dir))
         torch.save(GLOVE, self.path+'db%s/s%s/glove.pt'%(dbnum,p_dir))
 
-    def load_dataset(self):
+    # credit to github user parasgulati8
+    def filter(self, data, f, butterworth_order=4,btype="lowpass"):
+        nyquist=Hz/2
+        if isinstance(f, int):
+            fc = f/nyquist
+        else:
+            fc = list(f)
+            for i in range(len(f)):
+                fc[i] = fc[i]/nyquist
+        b,a = signal.butter(butterworth_order, fc, btype=btype)
+        transpose = data.T#.copy()
+        
+        for i in range(len(transpose)):
+            transpose[i] = (signal.lfilter(b, a, transpose[i]))
+        return transpose.T
+
+    def get_stim_rep(self, stimulus, repetition): # stimulus from 1-40, repetition from 1-6
+        global TASK_DIST
+        TASK_DIST=np.array(TASK_DIST)
+        ex=np.searchsorted(TASK_DIST.cumsum(), stimulus)
+        emg, acc, glove, stim, rep = self.Es[ex]
+        # emg, acc, glove, stimulus, repetition
+        stim_mask, rep_mask=(stim==stimulus), (rep==repetition) #(0 if stimulus==0 else repetition))
+        mask=(stim_mask&rep_mask).squeeze()
+        if mask.sum() < TOTAL_WINDOW_SIZE*(Hz//1000):
+            print("Smaller size: %s, stimulus %s, repetition %s"%(str(self.person), str(stimulus), str(repetition)))
+            time_mask=np.array([np.arange(0,Hz//1000*WINDOW_MS,Hz//1000,dtype=np.uint8)]*AMT_WINDOWS)
+            emg_=emg[mask][time_mask] * 16 # 15 ms window
+            acc_=acc[mask][time_mask] * 16
+            glove_=glove[mask][time_mask] * 16
+            emg_=emg_.reshape(-1, EMG_DIM)
+            acc_=acc_.reshape(-1, ACC_DIM)
+            glove_=glove_.reshape(-1, GLOVE_DIM)
+        else:
+            emg_=emg[mask][self.time_mask] * 16  # bit shift to avoid having too low of a number
+            acc_=acc[mask][self.time_mask] * 16
+            glove_=glove[mask][self.time_mask] * 16 # / 2**3
+
+        # filter
+        emg_=self.filter(emg_, (20, 40), butterworth_order=4,btype="bandpass") # bandpass filter
+
+        # take out outliers
+        iqr_glove=np.subtract(*np.percentile(glove_, [75, 25], axis=0))
+        iqr_acc=np.subtract(*np.percentile(acc_, [75, 25], axis=0))
+        emg_high,emg_low=np.percentile(acc_, [99, 1], axis=0)
+
+        for dim in range(GLOVE_DIM):
+            glove_.T[dim][(glove_.T[dim]>1.5*iqr_glove[dim])] = iqr_glove[dim]
+            glove_.T[dim][(glove_.T[dim]<-1.5*iqr_glove[dim])] = -iqr_glove[dim]
+
+        for dim in range(ACC_DIM):
+            acc_.T[dim][(acc_.T[dim]>1.5*iqr_acc[dim])] = iqr_acc[dim]
+            acc_.T[dim][(acc_.T[dim]<-1.5*iqr_acc[dim])] = -iqr_acc[dim]
+
+        for dim in range(EMG_DIM):
+            emg_.T[dim][(emg_.T[dim]>emg_high[dim])] = emg_high[dim]
+            emg_.T[dim][(emg_.T[dim]<emg_low[dim])] = -emg_low[dim]
+
+        acc_=acc_.reshape(AMT_WINDOWS, -1, ACC_DIM).mean(1)
+
+        return emg_, acc_, glove_
+
+
+    def load_dataset(self, norm=False):
         """
         Loads dataset as a pt file format all preprocessed.
-        subject -> reps -> amt windows -> window_ms (1 frame per ms) -> dim (emg,acc,glove)
+        subject -> reps -> stim -> amt windows -> window_ms (1 frame per ms) -> dim (emg,acc,glove)
         """
-        #PEOPLE=PEOPLE_D2+PEOPLE_D3
-        PEOPLE=[0,1]
+        PEOPLE=PEOPLE_D2+PEOPLE_D3
         self.time_mask=np.arange(0,Hz//1000*TOTAL_WINDOW_SIZE,Hz//1000,dtype=np.uint8)
 
-        emg_means,acc_means,glove_means=[],[],[]
-        emg_sq,acc_sq,glove_sq=[],[],[]
-        subject_totals=[]
+        self.emg_stats=RunningStats()
+        self.acc_stats=RunningStats()
+        self.glove_stats=RunningStats()
 
         for person in tqdm.tqdm(PEOPLE):
-            dbnum="3" if person >= MAX_PEOPLE_D2 else "2"
-            if dbnum=="2":
-                person %= MAX_PEOPLE_D2
-            p_dir=str(person+1)
-
-            # gestures go from 1 to 17, 1 to 23, 1 to 9, rest (0)
+            # gestures go from 1 to 17, 1 to 23, rest (0)
             # emg, acc, glove, stimulus, repetition
-            E1=get_np(dbnum,p_dir,"1")
-            E2=get_np(dbnum,p_dir,"2")
-            self.Es = (E1, E2)
-            subject_totals.append(len(E1[-1])+len(E2[-1]))
+            self.person=person
+            dbnum="3" if person >= MAX_PEOPLE_D2 else "2"
+            subject=person
+            if dbnum=="3":
+                subject %= MAX_PEOPLE_D2
+            p_dir=str(subject+1)
 
-            #huge_mask, huge_enum=[],[]
-            #for stimulus in 
-            shape=(MAX_TASKS+1,MAX_REPS,TOTAL_WINDOW_SIZE)
-            EMG=torch.empty(shape+(EMG_DIM,),device=self.device)
-            ACC=torch.empty(shape[:-1]+(AMT_WINDOWS, ACC_DIM,),device=self.device)
-            GLOVE=torch.empty(shape+(GLOVE_DIM,),device=self.device)
+            if not norm:
+                # Fetch from .mat file
+                E1=get_np(dbnum,p_dir,"1")
+                E2=get_np(dbnum,p_dir,"2")
+                self.Es = (E1, E2)
 
-            emg_means.append(EMG.mean((0,1,2)))
-            acc_means.append(ACC.mean((0,1,2)))
-            glove_means.append(GLOVE.mean((0,1,2)))
+                shape=(MAX_TASKS+1,MAX_REPS,TOTAL_WINDOW_SIZE)
+                EMG=torch.empty(shape+(EMG_DIM,),device=self.device)
+                ACC=torch.empty(shape[:-1]+(AMT_WINDOWS, ACC_DIM,),device=self.device)
+                GLOVE=torch.empty(shape+(GLOVE_DIM,),device=self.device)
 
-            emg_sq.append((EMG**2).mean((0,1,2)))
-            acc_sq.append((ACC**2).mean((0,1,2)))
-            glove_sq.append((GLOVE**2).mean((0,1,2)))
+                for rep in range(1,MAX_REPS+1):
+                    for stim in range(MAX_TASKS+1):
+                        emg,acc,glove=self.get_stim_rep(stim,rep)
+                        emg,acc,glove=torchize(emg),torchize(acc),torchize(glove)
 
-            for rep in range(1,MAX_REPS+1):
-                for stim in range(MAX_TASKS+1):
-                    emg,acc,glove=self.get_stim_rep(stim,rep)
-                    EMG[stim,rep-1]=torchize(emg)
-                    ACC[stim,rep-1]=torchize(acc)
-                    GLOVE[stim,rep-1]=torchize(glove)
+                        self.emg_stats.push(emg) 
+                        self.acc_stats.push(acc) 
+                        self.glove_stats.push(glove) 
 
-            EMG=EMG.reshape(EMG.shape[:-2]+(AMT_WINDOWS, WINDOW_MS, EMG_DIM))
-            GLOVE=GLOVE.reshape(GLOVE.shape[:-2]+(AMT_WINDOWS, WINDOW_MS, GLOVE_DIM))
+                        EMG[stim,rep-1]=emg
+                        ACC[stim,rep-1]=acc
+                        GLOVE[stim,rep-1]=glove
 
-            #save
-            self.save(person, (EMG,ACC,GLOVE))
+            else:
+                # Fetch from .pt file
+                EMG,ACC,GLOVE=self.load_subject(person)
 
-        sum_total=sum(subject_totals)
-        subject_totals=torchize(subject_totals) / sum_total
 
-        emg_means=torchize(emg_means * subject_totals).mean(0)
-        acc_means=torchize(acc_means * subject_totals).mean(0)
-        glove_means=torchize(glove_means * subject_totals).mean(0)
+            if not norm:
+                EMG=EMG.reshape(EMG.shape[:-2]+(AMT_WINDOWS, WINDOW_MS, EMG_DIM))
+                GLOVE=GLOVE.reshape(GLOVE.shape[:-2]+(AMT_WINDOWS, WINDOW_MS, GLOVE_DIM))
+                #save
+                self.save_subject(person, (EMG,ACC,GLOVE))
 
-        emg_sq=torchize(emg_sq * subject_totals).mean(0)
-        acc_sq=torchize(acc_sq * subject_totals).mean(0)
-        glove_sq=torchize(glove_sq * subject_totals).mean(0)
-
-        subject_totals *= sum_total
-        emg_std=torchize(torch.sqrt(  (emg_means*sum_total) - (emg_sq/sum_total)  ))
-        acc_std=torchize(torch.sqrt(  (acc_means*sum_total) - (acc_sq/sum_total)  ))
-        glove_std=torchize(torch.sqrt(  (glove_means*sum_total) - (glove_sq/sum_total)  ))
+        emg_means,emg_std=self.emg_stats.mean_std()
+        acc_means,acc_std=self.acc_stats.mean_std()
+        glove_means,glove_std=self.glove_stats.mean_std()
+        print(emg_std,acc_std,glove_std)
 
         # normalize
         for person in tqdm.tqdm(PEOPLE):
             
-            EMG,ACC,GLOVE=self.load(person)
-            EMG=(EMG+emg_means)/emg_std
-            ACC=(ACC+acc_means)/acc_std
-            GLOVE=(GLOVE+glove_means)/glove_std
+            EMG,ACC,GLOVE=self.load_subject(person)
+            EMG=(EMG-emg_means)/emg_std
+            ACC=(ACC-acc_means)/acc_std
+            GLOVE=(GLOVE-glove_means)/glove_std
             #save
-            self.save(person, (EMG,ACC,GLOVE))
+            self.save_subject(person, (EMG,ACC,GLOVE))
 
         print("Dataset (un)loading completed successfully")
+        return None
 
     def __getitem__(self, batch_idx):
-        # batch of constant size of tasks*2 (train time)
-        pass
+        # batch_idx -> rep_block x subject
+        # return batch of shape (2 (rep) * amtwindows * maxtask) x windowms x respective dim
+        # acc dimension is just a mean
+        # glove and emg are images (windowms x respective dim)
 
-db=DB23()
-t=time.time()
-print(len(db))
-db.load_dataset()
-print(time.time()-t)
+        BLOCK_SIZE=2
+        PEOPLE=MAX_PEOPLE_TRAIN if self.train else MAX_PEOPLE
+        TASKS=self.MAX_TASKS_TRAIN if self.train else MAX_TASKS
+
+        rep_block = batch_idx // PEOPLE
+        subject = batch_idx % PEOPLE 
+        
+
+        return self.load_subject(batch_idx)
+
+if __name__=="__main__":
+    t=time.time()
+    db=DB23()
+    db.load_dataset()
+    print(time.time()-t)
+
+    #t=time.time()
+    #for i in PEOPLE_D3+PEOPLE_D2:
+    #    EMG,ACC,GLOVE=db[i]
+    #    print(EMG.shape, i)
+    #print(time.time()-t)
